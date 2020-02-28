0cd69f01f81eda2afb5f076d83f8938c9551f6e6
Tomer Paz <tomer.paz@concertio.com>
2020-02-28 11:45:22 +0200

    add license env param to run script
    user can chose if to fill the license at build time in container image
    or at runtime when running docker container

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # run optimizer for stream.c compilation flag mining
-docker run --rm -ti --mount type=bind,source="$(pwd)",target=/opt/concertio-optimizer/studio/src optimize_stream --max-minutes=5 --workload-timeout=1m
+# if license was not provided in Dockerfile, can pass it here at runtime
+docker run --rm -ti -e OPTIMIZER_LICENSE_KEY --mount type=bind,source="$(pwd)",target=/opt/concertio-optimizer/studio/src \
+ optimize_stream --max-minutes=5 --workload-timeout=1m
