56dcbf1b410a92b62e809f729110c54a6e8aecd2
Tomer Paz <tomer.paz@concertio.com>
2020-03-01 18:28:14 +0200

    change ls to get more specific file name for report

--- locate_results_file.sh
+++ locate_results_file.sh
@@ -3,5 +3,5 @@
 # locate the Last opitmizer execution results JSON report
 # to be used to extract meaningful summary data to pipeline
 # such as improvement %
-ls -ltr ~/.concertio/*.json |tail -1 | awk '{ print $9 }'
+ls -ltr ~/.concertio/report_*.json |tail -1 | awk '{ print $9 }'
 
