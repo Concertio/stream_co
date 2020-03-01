fa4db8a1ecdfcb6171bc03d14ae39dff7d0d4966
Tomer Paz <tomer.paz@concertio.com>
2020-03-01 17:53:28 +0200

    parameterize json results output location

--- docker_run.sh
+++ docker_run.sh
@@ -4,7 +4,7 @@
 optimizer-studio-license --activate=${OPTIMIZER_LICENSE_KEY}
 
 # run the experiment
-optimizer-studio --knobs ${OPTIMIZER_HOME:-/opt/concertio-optimizer/studio}/knobs.yaml --max-minutes=${OPTIMIZER_MAX_MINUTES:-5} --workload-timeout=1m
+optimizer-studio --settings ${SOURCE_FOLDER}/settings.yaml --knobs ${OPTIMIZER_HOME:-/opt/concertio-optimizer/studio}/knobs.yaml --max-minutes=${OPTIMIZER_MAX_MINUTES:-5} --workload-timeout=1m
 
 # deactivate license
 optimizer-studio-license --deactivate
--- locate_results_file.sh
+++ locate_results_file.sh
@@ -3,5 +3,5 @@
 # locate the Last opitmizer execution results JSON report
 # to be used to extract meaningful summary data to pipeline
 # such as improvement %
-ls -ltr ~/.concertio/*.json |tail -1 | awk '{ print $9 }'
+ls -1 ${SOURCE_FOLDER}/*.json |tail -1
 
