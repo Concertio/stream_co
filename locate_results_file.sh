245e69dcb16b9deea510ff3b81f792a94e55a23c
Tomer Paz <tomer.paz@concertio.com>
2020-03-01 17:38:20 +0200

    fix bash script

--- locate_results_file.sh
+++ locate_results_file.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 
 # locate the Last opitmizer execution results JSON report
 # to be used to extract meaningful summary data to pipeline
 # such as improvement %
 ls -ltr ~/.concertio/*.json |tail -1 | awk '{ print $9 }'
+
