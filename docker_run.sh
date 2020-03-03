d1085d627981f7caefbce8d83c53c69ce0423284
tomerpaz76 <tomer.paz@concertio.com>
2020-03-03 19:40:28 +0200

    handle optimizer outputs
    instruct optimizer to report settings to static settings file
    output best configuration to flags.make file
    to be committed later in Jenkins pipeline

--- docker_run.sh
+++ docker_run.sh
@@ -4,7 +4,11 @@
 optimizer-studio-license --activate=${OPTIMIZER_LICENSE_KEY}
 
 # run the experiment
-optimizer-studio --knobs ${OPTIMIZER_HOME:-/opt/concertio-optimizer/studio}/knobs.yaml --max-minutes=${OPTIMIZER_MAX_MINUTES:-5} --workload-timeout=1m
+optimizer-studio --knobs ${OPTIMIZER_HOME:-/opt/concertio-optimizer/studio}/knobs.yaml --max-minutes=${OPTIMIZER_MAX_MINUTES:-5} --workload-timeout=1m --settings-script=/tmp/settings.sh
+
+# write best results to flags file for commit
+source /tmp/settings.sh valarray
+echo ${KNOB_VALUES_TUNED[*]} > flags.make
 
 # deactivate license
 optimizer-studio-license --deactivate
